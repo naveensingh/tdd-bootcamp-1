35c622e986ed91c59cedfbc4fa68f887036129cd
Navin Singh <navinks@thoughtworks.com>
2017-12-14 23:56:05 +0530

    Navin - Added more test cases to measurement comparator

--- src/MeasurementComparer.java
+++ src/MeasurementComparer.java
@@ -1,5 +1,27 @@
 public class MeasurementComparer {
-    public boolean compareTwoMeasurements(int firstValue, int secondValue) {
-        return firstValue == secondValue;
+    private final Unit unit;
+    private final int value;
+
+    MeasurementComparer(int value, Unit unit) {
+        this.value = value;
+        this.unit = unit;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MeasurementComparer secondMeasurement = (MeasurementComparer) o;
+        return this.convertToInches(this.value, this.unit) == secondMeasurement.convertToInches(secondMeasurement.value, secondMeasurement.unit);
+    }
+
+    private int convertToInches(int value, Unit unit) {
+        if (unit.equals(Unit.FEET))
+            return value * 12;
+        return value;
+    }
+
+    public int add(MeasurementComparer measurementTwo) {
+        return this.convertToInches(this.value, this.unit) + this.convertToInches(measurementTwo.value, measurementTwo.unit);
     }
 }
--- test/MeasurementComparatorTest.java
+++ test/MeasurementComparatorTest.java
@@ -1,25 +1,54 @@
+import org.junit.Assert;
 import org.junit.Test;
-
-import static junit.framework.TestCase.assertEquals;
 
 public class MeasurementComparatorTest {
 
     @Test
-    public void VerifyIfMeasurementIsSameWhenFirstMeasurementValueIsTwoAndSecondMeasurementValueIsTwo() {
-        MeasurementComparer measurementComparer = new MeasurementComparer();
-        boolean comparisonResult = measurementComparer.compareTwoMeasurements(2, 2);
-        assertEquals(true, comparisonResult);
+    public void verifyIfTwoFeetAndTwoFeetAreEqual() {
+        MeasurementComparer measurementOne = new MeasurementComparer(2, Unit.FEET);
+        MeasurementComparer measurementTwo = new MeasurementComparer(2, Unit.FEET);
+        Assert.assertEquals(measurementOne, measurementTwo);
+    }
+
+    @Test
+    public void verifyIfTwoInchesAndTwoInchesAreEqual() {
+        MeasurementComparer measurementOne = new MeasurementComparer(2, Unit.INCH);
+        MeasurementComparer measurementTwo = new MeasurementComparer(2, Unit.INCH);
+        Assert.assertEquals(measurementOne, measurementTwo);
+    }
+
+    @Test
+    public void shouldVerifyIfOneFeetEqualsToTwelveInch() {
+        MeasurementComparer measurementOne = new MeasurementComparer(1, Unit.FEET);
+        MeasurementComparer measurementTwo = new MeasurementComparer(12, Unit.INCH);
+        Assert.assertEquals(measurementOne, measurementTwo);
+    }
+
+    @Test
+    public void shouldVerifyIfOneInchNotEqualsToTwelveFeet() {
+        MeasurementComparer measurementOne = new MeasurementComparer(1, Unit.INCH);
+        MeasurementComparer measurementTwo = new MeasurementComparer(12, Unit.FEET);
+        Assert.assertNotEquals(measurementOne, measurementTwo);
+    }
+
+    @Test
+    public void verifyIfTwoFeetAndTwentyFourInchesAreEqual() {
+        MeasurementComparer measurementOne = new MeasurementComparer(2, Unit.FEET);
+        MeasurementComparer measurementTwo = new MeasurementComparer(24, Unit.INCH);
+        Assert.assertEquals(measurementOne, measurementTwo);
+    }
+
+    @Test
+    public void verifyIfTwoFeetAndTwoFeetIsFourtyEightInch() {
+        MeasurementComparer measurementOne = new MeasurementComparer(2, Unit.FEET);
+        MeasurementComparer measurementTwo = new MeasurementComparer(2, Unit.FEET);
+        Assert.assertEquals(new MeasurementComparer(48, Unit.INCH), new MeasurementComparer(measurementOne.add(measurementTwo), Unit.INCH));
     }
     @Test
-    public void VerifyIfMeasurementIsSameWhenFirstMeasurementValueIsFourAndSecondMeasurementValueIsFour() {
-        MeasurementComparer measurementComparer = new MeasurementComparer();
-        boolean comparisonResult = measurementComparer.compareTwoMeasurements(4, 4);
-        assertEquals(true, comparisonResult);
+    public void verifyIfTwoFeetAndTwentyInchesIsFourtyFourInches() {
+        MeasurementComparer measurementOne = new MeasurementComparer(2, Unit.FEET);
+        MeasurementComparer measurementTwo = new MeasurementComparer(20, Unit.INCH);
+        Assert.assertEquals(new MeasurementComparer(44, Unit.INCH), new MeasurementComparer(measurementOne.add(measurementTwo), Unit.INCH));
     }
-    @Test
-    public void VerifyIfMeasurementIsNotSameWhenFirstMeasurementValueIsTwoAndSecondMeasurementValueIsThree() {
-        MeasurementComparer measurementComparer = new MeasurementComparer();
-        boolean comparisonResult = measurementComparer.compareTwoMeasurements(2, 3);
-        assertEquals(false, comparisonResult);
-    }
+
 }
